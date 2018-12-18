c701784819adec6ea2d46c5491e799e166df39b7
Tong Hui Kang <tonghuikang@outlook.com>
2018-12-18 15:43:58 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+# script to install a working inference version of DrQA
+# assumes Ubuntu 16.04, 4vCPU 26GB memory, 60GB storage
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install python3-dev python-setuptools python3-pip python-pip -y
+sudo apt-get install build-essential git unzip -y
+
+git clone https://github.com/tonghuikang/DrQA
+cd DrQA
+sudo pip3 install -r requirements.txt
+sudo pip3 install torch torchvision
+sudo python3 setup.py develop
+
